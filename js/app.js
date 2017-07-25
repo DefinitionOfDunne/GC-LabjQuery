dd726b2bc026aaecf30f31ec0dc4a5b958c20fea
Dylan Dunne <dylanadunne@gmail.com>
2017-07-25 14:45:48 -0400

    added sum feature

--- js/app.js
+++ js/app.js
@@ -1,13 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
+      var totalO= 0;
     $('#add-button').click(function() {
-        $('.active-items').prepend('<li class="in-cart">' + $('#input-items').val() + '</li>');
-   });
-  	$('.active-items').on('click', 'li', function() {
-  		$(this).removeClass("active-items");
-  		$(this).addClass ("bought");	
-	});
-	$('#clear-button').click(function(){
-		$('.active-items').empty();
-		$('.bought').empty();
-	});
+      var arr = [];
+      var item = $('#input-items').val()
+      var price = $('#input-price').val()
+      var grocOb = { item: item, price: price };
+      var price = $('#input-price').val()
+      var totalOutput = document.getElementById('total');
+      arr.push(grocOb);
+      for (var i = 0; i < arr.length; i++) {
+        totalO += Number(arr[i].price);
+    }
+      total.innerHTML = 'Total Cost is $' + totalO;
+      $('.active-items').prepend('<li class="in-cart">' + $('#input-items').val() + " $" + $('#input-price').val() + '</li>');
+    });
+    $('.active-items').on('click', 'li', function() {
+        $(this).removeClass("active-items");
+        $(this).addClass("bought");
+    });
+    $('#clear-button').click(function() {
+        $('.active-items').empty();
+        $('.bought').empty();
+        $("#total").empty();
+        totalO = 0;
+    });
 });
+
+
+
